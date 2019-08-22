9ac8b708ac735d4db21a81f893a899f7975ed716
Abe <abe@humboldt.edu>
2019-08-22 13:39:28 -0700

    Added math.abs to "targetDist" because I forgot about negative numbers.

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 //each actors position. If yours is equal
 //or greater than any other actors, return false
 //Otherwise, return false
-
-const ghosts = [[1,0],[0,3],[0,1]]
-const target = [1,1]
-var targetDist = target[0] + target[1]
+const ghosts = [[1,9],[2,-5],[3,8],[9,8],[-1,3]]
+const target = [8,-10]
+var targetDist = Math.abs(target[0]) + Math.abs(target[1]);
 var lowest = 99999999;
 
 for(var ghost of ghosts)
 {
   let dist= Math.abs(target[0]-ghost[0]) +  Math.abs(target[1]-ghost[1])
-  console.log(Math.abs(target[1]-ghost[1]));
   if(dist < lowest)
   {
     lowest = dist;
     console.log(lowest);
   }
 }
+
+console.log(targetDist,lowest);
 
 if(targetDist < lowest)
 {
