6a3073f5780bc9e4debfd1336173b5d0d45d2c30
Abe <abe@humboldt.edu>
2019-08-22 13:24:15 -0700

    Put down a game plan in the comments to help guide my thinking.

---
+++ script.js
@@ -0,0 +1,5 @@
+//Current game plan (naive):
+//Calculate steps total steps till goal from
+//each actors position. If yours is equal
+//or greater than any other actors, return false
+//Otherwise, return false
